a8fff0732067e8bdc5af7666cbd199b571bc86cb
Krystian Gwizdała <krystiangw@Krystians-MacBook-Pro.local>
2014-10-05 22:47:38 +0200

    Version 0

---
+++ client/js/factories/User.js
@@ -0,0 +1,12 @@
+'use strict';
+Blog.factory('User', function(Restangular) {
+    var User;
+    User = {
+        create: function(user) {
+            return Restangular
+                .one('users')
+                .customPOST(user);
+        }
+    };
+    return User;
+})
